7f35d9c8001219e080256e9ceadfc931610fb5a6
HW-Lee <tony123930@yahoo.com.tw>
2017-10-20 10:02:21 +0800

    [Feature][FFT] twiddle factor generator

--- include/FFT.h
+++ include/FFT.h
@@ -1,5 +1,15 @@
 #ifndef __FFT_H__
 #define __FFT_H__
 
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+#include <complex>
+#include <cmath>
+
+typedef std::complex<double> complexdbl;
+
+void bitReversalSwap(std::vector<complexdbl> data);
+std::vector<complexdbl> getTwiddleFactors(uint16_t N);
 
 #endif
--- src/FFT.cpp
+++ src/FFT.cpp
@@ -1,8 +1,17 @@
 #ifndef __FFT_CPP__
 #define __FFT_CPP__
 
-void bitReversalSwap()
+#include "FFT.h"
+
+std::vector<complexdbl> getTwiddleFactors(uint16_t N)
 {
+    std::vector<complexdbl> twiddle_factors(N);
+    for (uint16_t i = 0; i < N; i++) {
+        double radius = 2*M_PI * i/N;
+        twiddle_factors[i] = complexdbl(cos(radius), sin(radius));
+    }
+
+    return twiddle_factors;
 }
 
 #endif
